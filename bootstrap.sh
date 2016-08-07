da8ae481afcc4c6605e40bb929d223bf3fcf09fb
Wes Malone <wes@mitsi.com>
2016-08-07 02:11:26 -0500

    bootstrap: add a bootstrapping script

---
+++ bootstrap.sh
@@ -0,0 +1,48 @@
+#! /bin/dash
+
+set -e # exit on errors
+
+# install packages
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install \
+   vim git zsh tmux \
+   curl mtr tree htop
+
+# generate keys
+if [ ! -e $HOME/.ssh/id_rsa ]; then
+   ssh-keygen -t rsa -b 2048
+fi
+echo "\n\nAdd this thing to github allowed keys\nhttps://github.com/settings/keys\n\n"
+cat ~/.ssh/id_rsa.pub
+echo "\n"
+read foo
+
+# plenv
+if [ ! -d $HOME/.plenv ]; then
+   git clone https://github.com/tokuhirom/plenv.git ~/.plenv
+   git clone https://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build/
+fi
+
+# homeshick
+if [ ! -d $HOME/.homesick ]; then
+   git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+fi
+
+# ohmyzsh
+# sh -c "$(curl --fail --silent --show-error --location https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+HS=$HOME/.homesick/repos/homeshick/bin/homeshick
+[ ! -d $HOME/.homesick/repos/oh-my-zsh ] && \
+   $HS clone --batch robbyrussell/oh-my-zsh
+
+[ ! -d $HOME/.homesick/repos/dotfiles ] && \
+   $HS clone --batch git@github.com:wesQ3/dotfiles.git
+$HS link --force
+
+chsh --shell /bin/zsh
+
+# vim
+[ ! -d $HOME/.vim/bundle/Vundle.vim ] && \
+   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir -p $HOME/.vim/undo
+mkdir -p $HOME/.vim/swap
