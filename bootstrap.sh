4b42103ba7cf030b26c970bbaa819aaf662d9068
Wes Malone <wes@mitsi.com>
2025-03-18 09:09:38 -0500

    bootstrap: add starship prompt

--- bootstrap.sh
+++ bootstrap.sh
@@ -46,13 +46,13 @@ fi
 
 # homeshick
 if [ ! -d $HOME/.homesick ]; then
-   git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+   git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 fi
 
 # ohmyzsh
 # sh -c "$(curl --fail --silent --show-error --location https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 [ ! -d $HOME/.oh-my-zsh ] && \
-   git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+   git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 
 HS=$HOME/.homesick/repos/homeshick/bin/homeshick
 [ ! -d $HOME/.homesick/repos/dotfiles ] && \
@@ -61,7 +61,7 @@ $HS link --force
 
 # fzf fuzzy finding
 [ ! -f ~/.fzf.zsh ] && \
-   git clone git://github.com/junegunn/fzf.git $HOME/.fzf && \
+   git clone https://github.com/junegunn/fzf.git $HOME/.fzf && \
    $HOME/.fzf/install --no-update-rc
 
 # fonts
@@ -86,4 +86,17 @@ mkdir -p $vimdir/autoload
 if [ ! -d $vimdir/bundle/vim-plug ]; then
    git clone https://github.com/junegunn/vim-plug.git $vimdir/bundle/vim-plug
    ln -s $vimdir/bundle/vim-plug/plug.vim $vimdir/autoload/plug.vim
+fi
+
+# starship prompt
+if ! command -v starship >/dev/null 2>&1; then
+   echo "Install starship prompt"
+   TMP_DIR=$(mktemp -d)
+   curl -sL "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"\
+      -o "$TMP_DIR/starship.tar.gz"
+   tar -xvf "$TMP_DIR/starship.tar.gz" -C "$TMP_DIR"
+   sudo mv "$TMP_DIR/starship" /usr/local/bin/
+   rm -rf "$TMP_DIR"
+
+   echo "Starship installation complete."
 fi
